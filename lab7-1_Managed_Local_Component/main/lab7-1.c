63ad92d2d4586970d3b32295c5274fc94957c645
Kittipac <67030025@kmitl.ac.th>
2025-10-31 19:13:31 +0700

    Create lab7-1.c

---
+++ lab7-1_Managed_Local_Component/main/lab7-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "sensor.h"
+
+static const char *TAG = "LAB7-1";
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, " Lab 7-1: Local Component Demo Started");
+    
+    // เรียกใช้ฟังก์ชันจาก local component
+    sensor_init();
+    
+    while(1) {
+        sensor_read_data();
+        sensor_check_status();
+        
+        ESP_LOGI(TAG, "----------------------------");
+        vTaskDelay(pdMS_TO_TICKS(3000));
+    }
+}
